24a6de71aa89930e0de74fe5286c431b73167acf
ashlynnpai <ashlynnpai@gmail.com>
2017-10-17 21:28:48 -0400

    fix board dimensions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 for (var i=0; i<9; i++) {  
-  mydiv = '<h1 style="height: 50px; width: 50px; border-style: dotted solid;" id="square' + i + '"></h1>';
+  mydiv = '<div class="eachsquare" id="square' + i + '"></div>';
   $('#board').append(mydiv);
   $('#square'+i).html('X');
 }
