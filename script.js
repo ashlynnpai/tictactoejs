8204f0e378372655de82172db06d6d5156bfa78a
ashlynnpai <ashlynnpai@gmail.com>
2017-10-17 21:17:24 -0400

    add border to board

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-for (var i=0; i<9; i++) {
-  mydiv = '<div><h1 id="square' + i + '"></h1></div>';
-  $('#board').append('<div><h1 id="square' + i + '"></div>');
-  console.log(mydiv);
+for (var i=0; i<9; i++) {  
+  mydiv = '<h1 style="height: 50px; width: 50px; border-style: dotted solid;" id="square' + i + '"></h1>';
+  $('#board').append(mydiv);
   $('#square'+i).html('X');
 }
