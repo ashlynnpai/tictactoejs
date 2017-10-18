d53ec02df4d65839cb18da753cf4039328014e15
ashlynnpai <ashlynnpai@gmail.com>
2017-10-17 21:01:26 -0400

    Revert "new board"
    This reverts commit f3fd849ddbe50514969fe5df5ee2641b87b6c21a.

--- script.js
+++
@@ -1,6 +0,0 @@
-for (var i=0; i<9; i++) {
-	mydiv = '<div><h1 id="square' + i + '"></h1></div>';
-  $('#board').append('<div><h1 id="square' + i + '"></div>');
-  console.log(mydiv);
-  $('#square'+i).html('X');
-}
