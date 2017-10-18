f3fd849ddbe50514969fe5df5ee2641b87b6c21a
ashlynnpai <ashlynnpai@gmail.com>
2017-10-17 20:57:57 -0400

    new board

---
+++ script.js
@@ -0,0 +1,6 @@
+for (var i=0; i<9; i++) {
+	mydiv = '<div><h1 id="square' + i + '"></h1></div>';
+  $('#board').append('<div><h1 id="square' + i + '"></div>');
+  console.log(mydiv);
+  $('#square'+i).html('X');
+}
