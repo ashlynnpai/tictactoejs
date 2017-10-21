311d99d681d313bac92b05abec6f0674452e7fb9
ashlynnpai <ashlynnpai@gmail.com>
2017-10-21 19:39:58 -0400

    check for wins

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var state;
+var playerMove = [];
+WINS = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
 
 for (var i=0; i<9; i++) {  
   mydiv = '<div class="eachsquare" id="square' + i + '"></div>';
@@ -12,5 +14,21 @@ $('.eachsquare').click(function() {
   state = 1;
 })
 
+function checkWins() {
+  var count = 0;
+    for (var i=0; i<WINS.length; i++) {
+      for (var j=0; j<WINS[i].length; j++) {  
+        if (playerMove.includes(WINS[i][j])) {
+          count++;
+      }
+    }
+    if (count == 3) {
+      console.log("win");
+    }
+    else {
+      count = 0;
+    }
+  }
+};
 
 
