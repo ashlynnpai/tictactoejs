98e466d7389ae1066e01ebfa18dc1a87da29ba9d
ashlynnpai <ashlynnpai@gmail.com>
2017-10-21 17:50:45 -0400

    setup X on click and game state

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+var state;
+
 for (var i=0; i<9; i++) {  
   mydiv = '<div class="eachsquare" id="square' + i + '"></div>';
   $('#board').append(mydiv);
-  $('#square'+i).html('X');
 }
+
+$('.eachsquare').click(function() {
+	if (state != 1) {
+		$(this).html('X');
+	}
+	state = 1;
+})
+
+
+
