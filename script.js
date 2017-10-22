8ec5e62ed9273d21ba1755af1a134d0cdb8a42fc
ashlynnpai <ashlynnpai@gmail.com>
2017-10-22 13:34:02 -0400

    add computer check for own two in a row and fix bugs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var state;
+var state; 
 var playerMove = [];
 var computerMove = [];
 var piece;
 var computerPiece;
 WINS = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+
 
 $( document ).ready(function() {
   startGame();
@@ -14,40 +15,53 @@ $( document ).ready(function() {
     playerMove.push(num);
     checkWins(playerMove);
     state = 1;
+
     setTimeout(function() {
-      computerTurn();
-      }, 3000);
+    computerTurn();
     checkWins(computerMove);
+    state = 0;
+    }, 1000);;
+    
   }
   });
 });
 
+  
+
+  
 function drawBoard() {
   $('#info').html('Tic Tac Toe');
-  for (var i=1; i<10; i++) {
-    mydiv = '<div class="eachsquare" id="square' + i + '"></div>';
-    $('#board').append(mydiv);
+    for (var i=1; i<10; i++) {
+      mydiv = '<div class="eachsquare" id="square' + i + '"></div>';
+      $('#board').append(mydiv);
   }
 }
 
 function startGame(){
   $('#board').empty();
+  state = null; 
+  playerMove = [];
+  computerMove = [];
+  piece = null;
+  computerPiece = null;
   drawBoard();
   $('#info').html('Choose a piece');
   $('button').click(function() {
-    piece = this.id;
-    if (piece = 'X') {
-      computerPiece = 'O';
-    }
-    else {
-      computerPiece = 'X';
-    }
+  piece = this.id;
+  if (piece == 'X') {
+    computerPiece = 'O';
+  }
+  else {
+    computerPiece = 'X';
+  }
   $('#info').html('You chose ' + piece + '. Choose a square.');
   state = 0;
   $('piece').hide();
   });
 }
 
+
+  
   function checkWins(arr) {
     var count = 0;
     for (var i=0; i<WINS.length; i++) {
@@ -78,18 +92,24 @@ function startGame(){
       }
     }
   };
-
+  
 function computerTurn() {
   var finalChoice;
-  if (!playerMove.includes(5) &&  !computerMove.includes(5)) {
+   if (!playerMove.includes(5) &&  !computerMove.includes(5)) {
    finalChoice = 5;
    }
-  else {
-    finalChoice = leftovers();
+   else {
+     var temp = two_in_a_row(computerMove);
+     console.log(temp);
+     if (!playerMove.includes(temp) &&  !computerMove.includes(temp)) {
+   finalChoice = temp;
    }
-  computerMove.push(finalChoice);
+    if (!finalChoice) {
+       finalChoice = leftovers();
+    } 
+   }
+   computerMove.push(finalChoice);
   $('#square' + finalChoice).html(computerPiece);
-    state = 0;
 }
 
   
@@ -104,3 +124,17 @@ function computerTurn() {
     }
   }
   
+function two_in_a_row(arr) {
+  var tempWins = WINS.slice();
+  for (var i=0; i<tempWins.length; i++) {
+    var row = tempWins[i].slice();
+    for (var j=0; j<row.length; j++) {
+      if (arr.includes(row[j])) {
+        row.splice(j, 1); 
+        if (row.length == 1) {
+          return row.shift();
+        }
+      }
+    }
+  }
+}
